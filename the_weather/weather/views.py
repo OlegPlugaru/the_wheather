d948aa34a7b8a72417f4422fc65b6a0de4e0ace2
OlegPlugaru <oleg.plugaru.md@gmail.com>
2023-01-27 16:08:10 +0200

    added a lot of changes

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import requests
+from django.shortcuts import render 
 
 # Create your views here.
+def index(request):
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=d416fabbdafa48e73001e8402addc5ab'
+    city = 'Chisinau'
+    r = requests.get(url.format(city))
+    print(r.text)
+    return render(request, 'weather/weather.html')
